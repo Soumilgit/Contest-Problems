0df9ff7b8e5b3d9b134ec1d53a1a18c2f326f6a5
Soumil Mukhopadhyay <soumil.m@somaiya.edu>
2024-04-25 00:19:31 +0530

    Create RemainingNeighbourhoods.cpp

---
+++ Codechef-Starters/RemainingNeighbourhoods.cpp
@@ -0,0 +1,13 @@
+/*Link:https://www.codechef.com/problems/NEIREM?tab=statement*/
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int N;
+	cin>>N;
+	cout<<100-N<<endl;
+	
+	return 0;
+
+}
