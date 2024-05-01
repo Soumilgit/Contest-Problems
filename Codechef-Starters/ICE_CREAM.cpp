894590479fa51b6afedb3d603031937942168b9f
Soumil Mukhopadhyay <soumil.m@somaiya.edu>
2024-05-01 22:25:50 +0530

    Add files via upload

---
+++ Codechef-Starters/ICE_CREAM.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int X,Y;
+	cin>>X>>Y;
+	if(Y/X>=2){
+	    cout<<"YES"<<endl;
+	}
+	else{
+	    cout<<"NO"<<endl;
+	}
+	return 0;
+
+}
